d8f3f00efa6aed20dc430211d286f36bb3f49dd7
heinthant <hthant00chk@gmail.com>
2023-05-16 16:38:19 +0630

    Fetch greeting from endpoint

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getRandomGreeting } from '../redux/greetings';
 
 function Greeting() {
-  const greeting = 'hello world';
+  const greeting = useSelector((state) => state.greeting);
+  const error = useSelector((state) => state.error);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRandomGreeting());
+  }, [dispatch]);
+
   return (
     <section className="page">
       <h1>Random Greeting</h1>
@@ -10,6 +19,7 @@ function Greeting() {
       <Link to="/">
         <button>Back to Home</button>
       </Link>
+      <small>{error}</small>
     </section>
   );
 }
